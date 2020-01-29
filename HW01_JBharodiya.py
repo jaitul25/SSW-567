eb1b2b05fe5416c926d9d92746580bfdfdedbcf3
jaitul25 <57580916+jaitul25@users.noreply.github.com>
2020-01-28 21:44:13 -0500

    Add files via upload

---
+++ HW01_JBharodiya.py
@@ -0,0 +1,24 @@
+"""Check if the triangle is isosceles, right , scalene or equilateral """
+
+def classify_triangle(a,b,c):
+    """Check the triangle"""
+
+    if a == b == c:
+        return 'Equilateral Triangle'
+
+    elif a == b or a == c or b == c:
+        return "Isosceles Triangle"
+
+    elif a*a + b*b  == c*c:
+        return "Right Angled Triangle"
+
+    elif a != b or b != c:
+        return "Scalene Triangle"
+
+def main():
+    a = int(input("Enter the length of side 1 : "))
+    b = int(input("Enter the length of side 2 : "))
+    c = int(input("Enter the length of side 3 : "))
+
+if __name__ == '__main__':
+    main()
---
+++ HW01_JBharodiya_Test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from HW01_JBharodiya import classify_triangle
+
+class TestTriangles(unittest.TestCase):
+
+    def testEquilateralTriangle1(self): 
+        self.assertEqual(classify_triangle(5,5,5),'Equilateral Triangle')
+
+    def testEquilateralTriangle2(self): 
+        self.assertEqual(classify_triangle(7,7,7),'Equilateral Triangle')
+
+    def testIsocelesTriangle3(self): 
+        self.assertEqual(classify_triangle(7,7,3),'Isosceles Triangle')
+    
+    def testIsocelesTriangle4(self):     
+        self.assertEqual(classify_triangle(5,7,7),'Isosceles Triangle')
+
+    def testRightTriangle5(self): 
+        self.assertEqual(classify_triangle(3,4,5),'Right Angled Triangle')
+
+    def testScaleneTriangleA(self): 
+        self.assertEqual(classify_triangle(7,8,9),'Scalene Triangle')
+
+if __name__ == '__main__':
+    unittest.main(exit=False,verbosity=2)
